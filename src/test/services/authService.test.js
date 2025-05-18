728ce5ae30e8912f776c474a1edf9a96219706d1
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-05-18 18:52:46 -0300

    feat: Adiciona mais testes para o cadastro de usuários

--- src/test/services/authService.test.js
+++ src/test/services/authService.test.js
@@ -1,5 +1,7 @@
 import { describe, expect } from '@jest/globals';
+import bcryptjs from 'bcryptjs';
 import AuthService from '../../services/authService';
+import Usuario from '../../models/usuario';
 
 const authService = new AuthService();
 
@@ -16,5 +18,49 @@ describe('Testando a authService.cadastrarUsuario', () => {
 
     // assert
     await expect(usuarioCadastrado).rejects.toThrowError('A senha é um campo obrigatório!');
+  });
+
+  it('A senha do usuário precisa ser criptografada quando for salva no banco de dados', async () => {
+    const usuarioMock = {
+      nome: 'Eduardo',
+      email: 'eduardo@email.com',
+      senha: 'Senha@123',
+    };
+
+    const usuarioCadastrado = await authService.cadastrarUsuario(usuarioMock);
+
+    const asSenhasSaoIguais = await bcryptjs.compare('Senha@123', usuarioCadastrado.content.senha);
+
+    expect(asSenhasSaoIguais).toStrictEqual(true);
+
+    await Usuario.excluir(usuarioCadastrado.content.id);
+  });
+
+  it('Ao cadastrar um usuário deve ser retornada uma mensagem informando que o cadastro foi realizado', async () => {
+    const usuarioMock = {
+      nome: 'Eduardo',
+      email: 'eduardo@email.com',
+      senha: 'Senha@123',
+    };
+
+    const usuarioCadastrado = await authService.cadastrarUsuario(usuarioMock);
+
+    expect(usuarioCadastrado.message).toEqual('usuario criado');
+
+    await Usuario.excluir(usuarioCadastrado.content.id);
+  });
+
+  it('Ao cadastrar um usuário, validar o retorno das informações do usuário.', async () => {
+    const usuarioMock = {
+      nome: 'Eduardo',
+      email: 'eduardo@email.com',
+      senha: 'Senha@123',
+    };
+
+    const usuarioCadastrado = await authService.cadastrarUsuario(usuarioMock);
+
+    expect(usuarioCadastrado.content).toMatchObject(usuarioMock);
+
+    await Usuario.excluir(usuarioCadastrado.content.id);
   });
 });
